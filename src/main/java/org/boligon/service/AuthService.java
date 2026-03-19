93d00bbba083f00a051cfe256789ff4c34f963a0
Filipe Boligon <filiperuizboligon9@gmail.com>
2026-03-19 00:16:25 -0300

    feat: métodos de registrar e login implementados no AuthService

--- src/main/java/org/boligon/service/AuthService.java
+++ src/main/java/org/boligon/service/AuthService.java
@@ -1,4 +1,42 @@
 package org.boligon.service;
 
+import org.boligon.entity.Usuario;
+import org.boligon.repository.UsuarioRepository;
+
 public class AuthService {
+
+    private final UsuarioRepository usuarioRepository = new UsuarioRepository();
+
+    public void registrar(String nome, String email, String senha) {
+        if(usuarioRepository.existePorEmail(email)) {
+            throw new RuntimeException("Já existe um usuário com o email vinculado.");
+        }
+
+        Usuario novoUsuario = new Usuario();
+        novoUsuario.setNome(nome);
+        novoUsuario.setEmail(email.trim().toLowerCase());
+        novoUsuario.setSenha(senha);
+        novoUsuario.setPerfil("CIDADAO");
+        novoUsuario.setAtivo(true);
+
+        usuarioRepository.salvar(novoUsuario);
+    }
+
+    public Usuario login(String email, String senha) {
+        Usuario usuario = usuarioRepository.buscarPorEmail(email.trim().toLowerCase());
+
+        if(usuario == null) {
+            throw new RuntimeException("Usuário não encontrado.");
+        }
+
+        if(!usuario.getSenha().equals(senha)) {
+            throw new RuntimeException("Senha inválida");
+        }
+
+        if(!usuario.getAtivo()) {
+            throw new RuntimeException("Usuário Inativo.");
+        }
+
+        return usuario;
+    }
 }
