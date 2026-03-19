6a1fc404b5c174c553b36b04cfb909b6cd327366
Filipe Boligon <filiperuizboligon9@gmail.com>
2026-03-19 14:48:57 -0300

    feat: criando enum pro perfil do usuário

--- src/main/java/org/boligon/entity/PerfilUsuario.java
+++ src/main/java/org/boligon/entity/PerfilUsuario.java
@@ -1,4 +1,17 @@
 package org.boligon.entity;
 
-public class PerfilUsuario {
+public enum PerfilUsuario {
+    CIDADAO("CIDADAO"),
+    GESTOR("GESTOR"),
+    ANONIMO("ANONIMO");
+
+    private final String valor;
+
+    PerfilUsuario(String valor) {
+        this.valor = valor;
+    }
+
+    public String getValor() {
+        return valor;
+    }
 }
